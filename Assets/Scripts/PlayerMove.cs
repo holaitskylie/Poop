b7712fc0ed6ca07d6b04bc6ae19587f7f9401c22
holaitskylie <117277650+holaitskylie@users.noreply.github.com>
2023-09-13 01:03:35 +0900

    Added Animation and Poops
    1. Added Player's Animation
    
    2. Spawning Poops

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 3f;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
     
     // Start is called before the first frame update
     void Start()
-    {        
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         
     }
 
@@ -17,10 +21,28 @@ public class PlayerMove : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            //왼쪽 이동
             transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+
+            //애니메이션 전환 조건 실행
+            animator.SetBool("Walk", true);
+
+            //이미지의 좌우반전
+            spriteRenderer.flipX = false;
         }
         else if(Input.GetKey(KeyCode.RightArrow)) {
+            //오른쪽 이동
             transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+
+            //애니메이션 전환 조건 실행
+            animator.SetBool("Walk", true);
+
+            //이미지의 좌우반전
+            spriteRenderer.flipX = true; 
+        }
+        else
+        {   //아무런 키 입력이 없는 경우, Idle 애니메이션 실행
+            animator.SetBool("Walk", false);
         }
         
     }
---
+++ Assets/Scripts/SlimeSpawner.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeSpawner : MonoBehaviour
+{
+    public GameObject[] slimes;
+    public float slimeInterval = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartSpawning();
+        
+    }
+
+    void StartSpawning()
+    {
+        StartCoroutine(SpawnSlimeRoutine());
+    }
+
+    void StopSpawning()
+    {
+        StopCoroutine(SpawnSlimeRoutine());
+    }
+
+    IEnumerator SpawnSlimeRoutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        while(true)
+        {
+            SpawnSlime();
+            yield return new WaitForSeconds(slimeInterval);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnSlime()
+    {
+        //슬라임이 떨어질 위치 지정
+        //x축의 값을 랜덤으로 지정한다
+        float posX = Random.Range(-3f, 3f);       
+        Vector3 position = new Vector3(posX, 6, 0);
+
+        //스폰할 슬라임 프리팹 생성
+        //랜덤으로 스폰할 슬라임 배열의 인덱스값 설정
+        int index = Random.Range(0, slimes.Length);
+        Instantiate(slimes[index], position, Quaternion.identity);
+    }
+}
